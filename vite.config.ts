70939ce606a33b39e9d2f98787b8453537ef0f9e
Oleksandr Shendryk <oleksandr.shendryk.ua@gmail.com>
2024-01-08 15:43:24 +0200

    change vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+        output: {
+            entryFileNames: '[name].[hash].js',
+            assetFileNames: '[name].[hash].[ext]',
+            chunkFileNames: 'chunk.[hash].js',
+        },
+    },
+},
   base: '/ts-react-tw-05-movies', // Базовый путь для обработки маршрутов
   plugins: [react()],
   define: {
